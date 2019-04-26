4e3d5af37cc100b7beed268719271155820aea69
Nikolay Moskal <nik211995@gmail.com>
2019-04-26 22:03:47 +0300

    feat(category): add many categories support

--- Program.cs
+++ Program.cs
@@ -24,6 +24,7 @@ namespace ThingWriter
             {
                 array = JArray.Parse(File.ReadAllText(@"all_things.json", Encoding.UTF8));
             }
+            int count = array.Count;
             var isContinue = true;
 
             while (isContinue)
@@ -56,28 +57,39 @@ namespace ThingWriter
                     case 13: thing.Type = "kids"; break;
                 }
                 Console.WriteLine("Category:");
-                Console.WriteLine("1 - Туризм\n2 - Велотуризм\n3 - Зима\n4 - Пляж\n5 - Командировка\n" +
-                    "6 - Дайвинг\n7 - Самое необходимое\n8 - Фотография\n9 - Рыбалка\n10 - Поход\n" +
-                    "11 - Туалет\n12 - Дети\n13 - Ресторан\n14 - Путешествие");
-                var category = Convert.ToInt32(Console.ReadLine());
-                switch (category)
+                bool isAddMore;
+                thing.Category = string.Empty;
+                do
                 {
-                    case 1: thing.Category = "tourism"; break;
-                    case 2: thing.Category = "cycle"; break;
-                    case 3: thing.Category = "winter"; break;
-                    case 4: thing.Category = "beach"; break;
-                    case 5: thing.Category = "business"; break;
-                    case 6: thing.Category = "swim"; break;
-                    case 7: thing.Category = "need"; break;
-                    case 8: thing.Category = "photo"; break;
-                    case 9: thing.Category = "fishing"; break;
-                    case 10: thing.Category = "hike"; break;
-                    case 11: thing.Category = "wc"; break;
-                    case 12: thing.Category = "kids"; break;
-                    case 13: thing.Category = "restaurant"; break;
-                    case 14: thing.Category = "travel"; break;
-                }
-                Console.WriteLine("Weight: ");
+                    Console.WriteLine("1 - Туризм\n2 - Велотуризм\n3 - Зима\n4 - Пляж\n5 - Командировка\n" +
+                        "6 - Дайвинг\n7 - Самое необходимое\n8 - Фотография\n9 - Рыбалка\n10 - Поход\n" +
+                        "11 - Туалет\n12 - Дети\n13 - Ресторан\n14 - Путешествие");
+                    var category = Convert.ToInt32(Console.ReadLine());
+                    switch (category)
+                    {
+                        case 1: thing.Category += "tourism"; break;
+                        case 2: thing.Category += "cycle"; break;
+                        case 3: thing.Category += "winter"; break;
+                        case 4: thing.Category += "beach"; break;
+                        case 5: thing.Category += "business"; break;
+                        case 6: thing.Category += "swim"; break;
+                        case 7: thing.Category += "need"; break;
+                        case 8: thing.Category += "photo"; break;
+                        case 9: thing.Category += "fishing"; break;
+                        case 10: thing.Category += "hike"; break;
+                        case 11: thing.Category += "wc"; break;
+                        case 12: thing.Category += "kids"; break;
+                        case 13: thing.Category += "restaurant"; break;
+                        case 14: thing.Category += "travel"; break;
+                    }
+                    Console.Write("Add one more category? [1-yes/0-no]: ");
+                    isAddMore = Convert.ToInt32(Console.ReadLine()) == 1;
+                    if (isAddMore)
+                    {
+                        thing.Category += ",";
+                    }
+                } while (isAddMore);
+                Console.Write("Weight: ");
                 thing.Weight = Convert.ToDouble(Console.ReadLine());
 
                 array.Add(JObject.FromObject(thing, serializer));
